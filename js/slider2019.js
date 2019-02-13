111bb5c436eda3c4a4ef7ea3a44052382ded693f
mihai <mihaiandrei_99@yahoo.com>
2019-02-13 20:36:53 +0200

    trebuie puse regulile si jocurile noi

---
+++ js/slider2019.js
@@ -0,0 +1,12 @@
+
+
+function addSelected(elem)
+{
+    var imgs = document.getElementsByClassName("carousel-indicator")[0].getElementsByTagName("img");
+    for(var i=0;i<imgs.length;i++)
+         {
+            imgs[i].classList.remove("selected");
+}
+     elem.classList.add("selected");
+    
+}
