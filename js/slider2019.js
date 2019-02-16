06aefc46a17ebb20837d9c2977c67a747ac44746
Ovidiu-Mihai Belciug <ovidiumihaibelciug@gmail.com>
2019-02-16 15:45:51 +0200

    regulament bug fixes, slider changes, bootstrap 3->4

--- js/slider2019.js
+++ js/slider2019.js
@@ -1,12 +1,8 @@
+function addSelected(elem) {
+  var imgs = document.getElementsByClassName("carousel-indicator")[0].getElementsByTagName("img");
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].classList.remove("selected");
+  }
+  elem.classList.add("selected");
 
-
-function addSelected(elem)
-{
-    var imgs = document.getElementsByClassName("carousel-indicator")[0].getElementsByTagName("img");
-    for(var i=0;i<imgs.length;i++)
-         {
-            imgs[i].classList.remove("selected");
-}
-     elem.classList.add("selected");
-    
 }
